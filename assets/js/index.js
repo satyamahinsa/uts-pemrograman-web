aba8427f25e509f578e037514bbf30618bc3ab5b
Gede Satyamahinsa <satyamahinsaa.pu@gmail.com>
2024-04-26 11:52:57 +0700

    finalisasi code part 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,19 +3,19 @@ const scrollToTopButton = document.getElementById('btn-scroll-to-top');
 // Fungsi untuk menentukan kapan tombol ditampilkan atau disembunyikan
 window.onscroll = function() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopButton.style.display = "block"; // Pastikan tombol terlihat
+        scrollToTopButton.style.display = "block";
         setTimeout(() => {
             scrollToTopButton.style.opacity = 1;    
         }, 500);
     } else {
-        scrollToTopButton.style.opacity = 0;       // Ubah opasitas menjadi 0
+        scrollToTopButton.style.opacity = 0;
         setTimeout(() => {
-            scrollToTopButton.style.display = "none"; // Sembunyikan tombol setelah transisi
-        }, 500); // Sesuaikan waktu dengan durasi transisi
+            scrollToTopButton.style.display = "none";
+        }, 500);
     }
 };
 
-// Fungsi untuk menggulir kembali ke atas
+// Fungsi untuk kembali ke atas
 scrollToTopButton.onclick = function() {
     window.scrollTo({
         top: 0,
